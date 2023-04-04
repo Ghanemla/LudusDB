c84114d0a0697153c234dc6bc5fcfb4d1e37c495
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-04-04 13:36:21 +0200

    added service for fetching user data from  API

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+
+import { User } from '../user';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  isLoggedIn = false;
+
+  configUrl = 'http://127.0.0.1:8000/api/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ',
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+}
