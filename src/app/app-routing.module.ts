2a7f1218dc47dc524bc104c50cfb350f0ba91dbd
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-04-04 11:52:26 +0200

    added routing and modules in app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { DetailsComponent } from "./components/details/details.component";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'search/:game-search', component:HomeComponent},
+  { path: 'details/:id', component:DetailsComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
