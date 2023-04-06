7af117f7aa0cea8996735fb1026aad00a7ceced7
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-04-06 11:40:01 +0200

    Bad practice with api key but only fix  :(

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,6 @@ import { apiEnv } from './api.env';
 export const environment = {
   production: true,
   BASE_URL: 'https://api.rawg.io/api',
-  api_key: apiEnv.api_key,
+  // api_key: apiEnv.api_key,
+  api_key: 'd429fdf9d6c74fd6a4b6d3fa3f907cac',
 };
